198bf2f7636e8384d1fe35ceabe7fe547acfb98b
schmandylyon <notaj@tutanota.de>
2021-12-11 18:14:10 +0300

    Add test to check that errors in credit card numbers are caught

--- 4_credit/test_credit.py
+++ 4_credit/test_credit.py
@@ -1,2 +1,12 @@
-import credit
+from credit import number_contains_errors
 import unittest
+
+
+class CreditCardChecker(unittest.TestCase):
+
+	def test_catching_errors_in_number(self):
+		# Checks that invalid characters within the number are caught
+		self.assertFalse(number_contains_errors("3782 82246 310005"))
+		self.assertTrue(number_contains_errors("3782 /82246 310005"))
+		self.assertFalse(number_contains_errors("378282246 310005"))
+		self.assertTrue(number_contains_errors("378_282246 310005"))
