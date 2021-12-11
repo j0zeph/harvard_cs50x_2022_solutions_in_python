35edb15951a0b631c276a826dab530d3836a6026
schmandylyon <notaj@tutanota.de>
2021-12-12 00:13:28 +0300

    Use regex to search for invalid characters instead of looping.

--- 4_credit/credit.py
+++ 4_credit/credit.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
+import re
+
+
 def number_contains_errors(number) -> bool:
 	"""Checks whether the number provided has any non-numeric characters in it.
 	Spaces do not count as invalid."""
 
-	invalid_characters = "(){}/*+-!@#$%^&_=`~,.?\\\"\'/;:|"
 	has_invalid = False
-	for character in number:
-		if character in invalid_characters:
-			has_invalid = True
-
+	found = re.search("[a-zA-Z/*+_{}()-/!@#$%^&=`~<>?|]", number)
+	if found:
+		has_invalid = True
 	return has_invalid
