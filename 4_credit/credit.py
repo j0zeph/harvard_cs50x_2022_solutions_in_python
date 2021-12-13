75b0a64385d00eeefaac1e5652ccf35810429fc7
schmandylyon <notaj@tutanota.de>
2021-12-13 09:21:02 +0300

    Make code in credit.py into a class structure

--- 4_credit/credit.py
+++ 4_credit/credit.py
@@ -1,37 +1,56 @@
 #!/usr/bin/env python3
 
 import re
+import list_utils
 
 
-def number_contains_errors(number) -> bool:
-	"""Checks whether the number provided has any non-numeric characters in it.
-	Spaces do not count as invalid."""
+class CreditCardChecker:
 
-	has_invalid = False
-	found = re.search("[a-zA-Z/*+_{}()-/!@#$%^&=`~<>?|]", number)
-	if found:
-		has_invalid = True
-	return has_invalid
+	def __init__(self, card_number):
+		self.credit_as_string = card_number
+		self.credit_as_number_list = self.make_into_ints()
 
+	# def make_card(self, card_number):
+	# 	self.credit_as_string = card_number
 
-def make_single_numbers(string_to_parse) -> list:
-	"""Returns a list of integers, as a result of parsing the provided string"""
-	parsed_list = []
-	for item in string_to_parse:
-		if item == " ":
-			continue
+	def has_errors(self) -> bool:
+		"""Checks whether this credit card number contains invalid characters.
+		Spaces are allowed"""
+
+		has_invalid_character = False
+		found = re.search("[a-zA-Z/*+_{}()-/!@#$%^&=`~<>?|]", self.credit_as_string)
+		if found:
+			has_invalid_character = True
+		return has_invalid_character
+
+	def make_into_ints(self) -> list:
+		"""Returns a list of integers after parsing the string
+		that represents this credit card number.
+		Returns an empty list if the string contains invalid characters"""
+
+		list_of_ints = []
+		if not self.has_errors():
+			for item in self.credit_as_string:
+				if item == " ":
+					continue
+				else:
+					try:
+						list_of_ints.append(int(item))
+					except ValueError:  # In case an invalid character was missed.
+						print("There was an error in the provided number.")
+						print("Please try again.\n")
+						break
+			return list_of_ints
 		else:
-			try:
-				parsed_list.append(int(item))
-			except ValueError:  # In case an invalid character was missed.
-				print("There was an error in the provided number.")
-				print("Please try again.\n")
-				break
-	return parsed_list
+			return []
 
+	def split_by_every_other_from_end(self) -> None:
+		pass
 
-def twice_every_other_from_second_last(list) -> list:
-	"""Returns a new list of numbers that are the result of multiplying
-	every other number in the provided list--starting from the second last
-	number--by 2"""
-	pass
+	def is_valid_credit_card_number(self) -> bool:
+		"""Returns whether or not this credit card number provided is a valid one"""
+		pass
+
+	def get_card_issuer(self):
+		"""Returns the card issuer(if any), of this valid credit card"""
+		pass
