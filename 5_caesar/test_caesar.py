63f9006580814bd323cf595661c168c008543a83
schmandylyon <notaj@tutanota.de>
2021-12-24 13:20:07 +0300

    Add test to check conversion of upper case letters.

--- 5_caesar/test_caesar.py
+++ 5_caesar/test_caesar.py
@@ -13,3 +13,13 @@ class CaesarTesting(unittest.TestCase):
         expected = ['f', 'r', 'k', 'p', 'z']
         for char, shifted in zip(chars_to_shift, expected):
             self.assertEqual(shifted, caesar.shift_by_key(char))
+
+    def test_shifting_upper_case_letters_by_a_small_amount(self):
+        """Checks that shifting an upper case letter by a small amount gives the
+        correct result, while preserving case."""
+
+        caesar = Caesar(10)
+        chars_to_shift = ['A', 'M', 'E', 'P']
+        expected = ['K', 'W', 'O', 'Z']
+        for char, shifted in zip(chars_to_shift, expected):
+            self.assertEqual(shifted, caesar.shift_by_key(char))
