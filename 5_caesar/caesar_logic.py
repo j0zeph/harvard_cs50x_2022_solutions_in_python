0b869bc442152140ffa44e6d8200d28689c9e49a
schmandylyon <notaj@tutanota.de>
2021-12-24 10:39:44 +0300

    Modify shift_by_key() function to pass unittest.

--- 5_caesar/caesar_logic.py
+++ 5_caesar/caesar_logic.py
@@ -5,10 +5,21 @@ from string import ascii_lowercase, ascii_uppercase
 
 class Caesar:
 
-    def __init__(self, key):
-        self.key = key
+    def __init__(self, key_to_shift_by):
+        self.key = key_to_shift_by
+
 
     def shift_by_key(self, char) -> str:
-        """Returns a string that represents a char, which is a result of
+        """Returns a string that represents a character, which is a result of
         shifting the provided char by `key` times."""
-        pass
+
+        # do not shift non-letters
+        if not char.isalpha():
+            return char
+
+        ASCII_value = ord(char)
+
+        shifted_char = (((ASCII_value + self.key) - 97) % 26) + 97
+        if char.isupper():
+            return chr(shifted_char.upper())
+        return chr(shifted_char)
