71fff21917ac9d0fb7fc40b18b9613e4332a37a3
schmandylyon <notaj@tutanota.de>
2021-12-24 11:05:25 +0300

    Update docstring and function name for more clarity.

--- 5_caesar/test_caesar.py
+++ 5_caesar/test_caesar.py
@@ -4,9 +4,9 @@ from caesar_logic import Caesar
 
 class CaesarTesting(unittest.TestCase):
 
-    def test_shifting_by_a_small_amount(self):
-        """Checks that shifting by a small amount gives the correct result.
-        Shifting preserves case."""
+    def test_shifting_lower_case_letters_by_a_small_amount(self):
+        """Checks that shifting a lower case letter by a small amount gives the
+        correct result, while preserving case."""
 
         caesar = Caesar(5)
         chars_to_shift = ['a', 'm', 'f', 'k', 'u']
