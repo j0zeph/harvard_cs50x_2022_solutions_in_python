1d288a25c04695809a85a5ee67bb7bc91682976d
schmandylyon <notaj@tutanota.de>
2021-12-24 15:35:12 +0300

    Update docstrings for more clarity.

--- 4_credit/list_utils.py
+++ 4_credit/list_utils.py
@@ -7,10 +7,12 @@ def add_numbers_in_list(list_to_total) -> int:
 
 
 def split_double_digits_into_singles(list_to_split) -> list:
-	"""Returns a new list in which all double-digit numbers have been split
-	into single-digit numbers, and put back into the list, at the
-	appropriate positions.
-	[1, 23, 7] becomes [1, 2, 3, 7], for example"""
+	"""Returns a new list in which all double-digit numbers in the provided list
+	have been split into single-digit numbers.
+	The new list maintains the order of digits in the provided list.
+
+	[1, 23, 7] returns [1, 2, 3, 7], and
+	[11, 34, 8] returns [1, 1, 3, 4, 8]"""
 
 	single_digits = []
 	for number in list_to_split:
@@ -37,11 +39,15 @@ def double_numbers_in_list(list_to_multiply) -> list:
 
 def get_every_other_from_end(list_to_split, starting_index) -> list:
 	"""Returns a list that is a result of getting every other element--starting
-	from the starting index, going backwards--in the provided list.
-	If the starting index is -2 (the second-last index), [1, 2, 3, 4, 5, 6]
-	returns [1, 3, 5].
-	[1, 2, 3, 4, 5, 6] returns [2, 4, 6] if the starting index is -1 (the
-	last index)"""
+	from some negative index, going backwards--in the provided list.
+
+	If the starting index is -2 (the second-last index) then:
+	[1, 2, 3, 4, 5, 6] returns [1, 3, 5], and
+	[7, 8, 9, 19] returns [7, 9].
+
+	If the starting index is -1 (the last index) then:
+	[1, 2, 3, 4, 5, 6] returns [2, 4, 6], and
+	[7, 8, 9, 19] returns [7, 9]."""
 
 	every_other_from_end = []
 	list_length = len(list_to_split)
