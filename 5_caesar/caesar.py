cec165fdfdeb2d98c68b105e2bbc2940d6c9da1d
schmandylyon <notaj@tutanota.de>
2021-12-24 16:34:35 +0300

    Add main() function and make the argv check its own function.

--- 5_caesar/caesar.py
+++ 5_caesar/caesar.py
@@ -2,7 +2,25 @@
 
 import caesar_utils as cu
 from sys import argv
+from caesar_logic import Caesar
 
-if not cu.argv_is_valid(argv):
-    cu.remind_user_of_usage()
-    exit(1)
+
+def main():
+    check_argv()
+    key = argv[1]
+    caesar = Caesar(int(key))
+    plain_text = input("plaintext: ")
+    print("ciphertext: ", end="")
+
+    for char in plain_text:
+        print(caesar.shift_by_key(char), end="")
+
+
+def check_argv() -> None:
+    if not cu.argv_is_valid(argv):
+        cu.remind_user_of_usage()
+        exit(1)
+
+
+if __name__ == "__main__":
+    main()
