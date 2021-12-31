61302eee3ea02fb4b05bc2241e343c38690249dd
schmandylyon <notaj@tutanota.de>
2021-12-31 17:54:36 +0300

    Add skeleton code for dictionary.py

---
+++ 6_speller/dictionary.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+loaded_words = ()
+SMALL_DICTIONARY_FILE = "dictionaries/small.txt"
+LARGE_DICTIONARY_FILE = "dictionaries/large.txt"
+
+
+def load() -> None:
+    """Loads all the words from a dictionary file into word_dictionary"""
+    pass
+
+
+def hash_function():
+    pass
+
+
+def size() -> int:
+    """Returns how many words are in the dictionary."""
+    pass
+
+
+def check():
+    """Checks if a word is in the dictionary."""
+    pass
+
+
+def unload():
+    pass
