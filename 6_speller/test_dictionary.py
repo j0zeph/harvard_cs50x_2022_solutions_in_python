56e1487fc91d78578ef9e87e9d8cdbcf08567ea2
schmandylyon <notaj@tutanota.de>
2022-01-01 12:28:54 +0300

    Add test to check that a loaded dictionary returns the correct size.

--- 6_speller/test_dictionary.py
+++ 6_speller/test_dictionary.py
@@ -3,6 +3,10 @@ import unittest
 
 class DictionaryTester(unittest.TestCase):
 
+    def setUp(self):
+        self.dictionary_path = "dictionaries/small_modified"
+        self.loaded_words = set()
+
     def test_that_dictionary_files_exist_in_expected_path(self):
         """Checks that the dictionary files can be found in their paths."""
 
@@ -16,3 +20,14 @@ class DictionaryTester(unittest.TestCase):
                 file_exists = False
 
             self.assertTrue(file_exists)
+
+    def test_that_a_loaded_dictionary_returns_the_correct_size(self):
+        """Checks that the loaded dictionary file returns the correct word
+        count.
+        Assumption: The dictionary contains only one word per line."""
+
+        with open(self.dictionary_path) as word_dict:
+            for line in word_dict:
+                self.loaded_words.add(line)
+
+        self.assertEqual(501, len(self.loaded_words))
