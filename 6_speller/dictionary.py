487504c2e56abb1a66152c891204495d07e7bdc2
schmandylyon <notaj@tutanota.de>
2022-01-01 19:48:59 +0300

    Update load() to reflect specification in the problem description.

--- 6_speller/dictionary.py
+++ 6_speller/dictionary.py
@@ -3,12 +3,16 @@
 loaded_words = set()
 
 
-def load(dictionary) -> None:
-    """Loads all the words from a dictionary file into word_dictionary"""
-
-    with open(dictionary) as word_dict:
-        for word in word_dict:
-            loaded_words.add(word)
+def load(dictionary) -> bool:
+    """Loads all the words from a dictionary file into word_dictionary.
+    Returns True if loading was successful, False otherwise."""
+    try:
+        with open(dictionary, encoding="utf-8") as word_dict:
+            for word in word_dict:
+                loaded_words.add(word)
+        return True
+    except FileNotFoundError:
+        return False
 
 
 def hash_function():
