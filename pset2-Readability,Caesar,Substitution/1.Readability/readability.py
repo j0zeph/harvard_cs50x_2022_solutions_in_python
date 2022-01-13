9cb2a2452787d27c33ecae4a13881fb22589844f
schmandylyon <notaj@tutanota.de>
2022-01-13 14:10:52 +0300

    Create functions to take inputted text, and count the letters.

--- pset2-Readability,Caesar,Substitution/1.Readability/readability.py
+++ pset2-Readability,Caesar,Substitution/1.Readability/readability.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3
+
+import re
+
+
+def get_text() -> str:
+    """Gets (as user input), the text whose reading level is to be determined"""
+
+    text = input("Text: ")
+    return text
+
+
+def count_letters(text: str) -> int:
+    """Counts the letters in the provided text"""
+
+    letter_count = 0
+
+    for char in text:
+        if re.match(r"[A-Za-z]", char):
+            letter_count += 1
+
+    return letter_count
