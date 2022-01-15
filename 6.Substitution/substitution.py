cf77a7de18473ea906622f33dabb0e509dc83190
schmandylyon <notaj@tutanota.de>
2022-01-15 08:28:35 +0300

    Remove unnecessary functions, and implement some functionality.
    Functions removed are:
    argv_is_valid(), key_is_valid() and show_usage_reminder().
    
    These would've had one-liner code, that need not have been in a function.

--- 6.Substitution/substitution.py
+++ 6.Substitution/substitution.py
@@ -1,20 +1,26 @@
 import sys
 
+CORRECT_ARGV_LENGTH = 2
+CORRECT_KEY_LENGTH = 26
+
 
 def main():
-    pass
+    if not(len(sys.argv) == CORRECT_ARGV_LENGTH):
+        exit(show_error("usage"))
+    elif not(len(sys.argv[1]) == CORRECT_KEY_LENGTH):
+        exit(show_error("key length"))
 
 
-def argv_is_valid() -> bool:
-    pass
+def show_error(message_type: str) -> str:
+    """Returns the error message that is associated with the
+    provided message type."""
 
+    messages = {
+        "usage": "Usage: python substitution.py key",
+        "key length": f"Key must contain {CORRECT_KEY_LENGTH} characters.",
+    }
 
-def key_is_valid(key: str) -> bool:
-    pass
-
-
-def show_usage_reminder() -> str:
-    pass
+    return messages[message_type]
 
 
 if __name__ == "__main__":
