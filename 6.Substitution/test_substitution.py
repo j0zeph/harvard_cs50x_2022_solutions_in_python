29aa67299f12c5224b791f223c84f8c5c0353111
schmandylyon <notaj@tutanota.de>
2022-01-16 16:57:45 +0300

    Implement test-cases, while using different variations of the same key.

--- 6.Substitution/test_substitution.py
+++ 6.Substitution/test_substitution.py
@@ -5,25 +5,35 @@ import substitution_class as substitution
 class SubstitutionTest(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.key_lower = "gtkylxjuzicpmnaqvowsehfrdb"
+        self.keys = {
+            "key_upper": "GTKYLXJUZICPMNAQVOWSEHFRDB",
+            "key_lower": "gtkylxjuzicpmnaqvowsehfrdb",
+            "key_mixed": "gTkYLxjuZIcPmNaQvOwSeHfRdB",
+        }
         self.sub = substitution.Substitution()
 
     def test_that_case_is_preserved_when_enciphering(self):
-
         plain_and_cipher = {
-            "This is almost CS50.": "Suzw zw gpmaws KW50."
+            "This is almost CS50.": "Suzw zw gpmaws KW50.",
+            "Almost, bUt not ReaLLy!": "Gpmaws, tEs nas OlgPPd!",
+            "ABcdEFghIJklMNopQRstUVwxYZ": "GTkyLXjuZIcpMNaqVOwsEHfrDB",
         }
 
         for plain, enciphered in plain_and_cipher.items():
+            self.sub.clear_ciphertext()
             self.sub.set_plaintext(plain)
-            self.sub.set_key(self.key_lower)
+            self.sub.set_key(self.keys["key_lower"])
             self.sub.encipher()
             self.assertEqual(self.sub.get_ciphertext(), enciphered)
 
     def test_that_upper_lower_and_mixed_case_keys_encipher_the_same(self):
-        pass
+        plaintext = "Click inside the Terminal window."
+        enciphered = "Kpzkc znwzyl sul Slomzngp fznyaf."
 
-    def test_that_symbols_are_not_enciphered(self):
-        pass
-
+        for value in self.keys.values():
+            self.sub.clear_ciphertext()
+            self.sub.set_plaintext(plaintext)
+            self.sub.set_key(self.keys["key_lower"])
+            self.sub.encipher()
+            self.assertEqual(self.sub.get_ciphertext(), enciphered)
 
