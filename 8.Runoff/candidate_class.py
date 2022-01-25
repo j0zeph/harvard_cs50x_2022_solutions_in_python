8a1f6aa40d15a09a4b2bbd5aee506e888ecf62c9
schmandylyon <notaj@tutanota.de>
2022-01-25 07:50:57 +0300

    Remove unnecessary type-hints for variables.

--- 8.Runoff/candidate_class.py
+++ 8.Runoff/candidate_class.py
@@ -2,6 +2,6 @@ class Candidate:
     """A candidate has a name, votes, and an `eliminated` status."""
 
     def __init__(self, name: str) -> None:
-        self.name: str = name
-        self.votes: int = 0
-        self.eliminated: bool = False
+        self.name = name
+        self.votes = 0
+        self.eliminated = False
