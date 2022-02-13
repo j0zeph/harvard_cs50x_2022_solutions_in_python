98e85c84ec6fe93a294ff514b325fbfaaec85995
schmandylyon <notaj@tutanota.de>
2022-02-13 09:30:07 +0300

    Add code for mario.

---
+++ 2.C++_solutions/1.Mario/1.mario_less_comfortable.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int height;
+
+    do
+    {
+        cout << "Height: ";
+        cin >> height;
+        
+        // ignore faulty input
+        if(!cin)
+        {
+            cin.clear();
+            cin.ignore(500, '\n');
+        }
+    }
+    while (!(height >= 1 && height <= 8));
+
+    int spaces;
+    int hashes;
+
+
+    for (int row = 0; row < height; row++)
+    {
+        spaces = height - (row + 1);
+
+        while (spaces > 0)
+        {
+            cout << " ";
+            spaces--;
+        }
+
+        hashes = (row + 1);
+
+        while (hashes > 0)
+        {
+            cout << "#";
+            hashes--;
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
