8a942d9a4f3ad9c26c127d1ec9d5d52e703f19ee
schmandylyon <notaj@tutanota.de>
2022-02-17 18:52:18 +0300

    Add argv error-checking for Caesar

---
+++ 2.C++_solutions/5.Caesar/Caesar.cpp
@@ -0,0 +1,38 @@
+#include <cctype>
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+bool argvIsValid(int argc, char *argv[]);
+
+int main(int argc, char *argv[])
+{
+    if (!(argvIsValid(argc, argv)))
+    {
+        cout << "Usage: ./caesar key";
+        return -1;
+    }
+    
+    return 0;
+
+}
+
+// Returns whether the command-line arguments provided
+// are valid.
+bool argvIsValid(int argc, char *argv[])
+{
+    if (!(argc == 2))
+        return false;
+    
+    // Check that the key supplied is numeric, by checking every character of the key.
+    string key = argv[1];
+    string::size_type length = key.length();
+
+    for (int i = 0; i < static_cast<int>(length); i++)
+        if (!isdigit(key[i]))
+            return false;
+    
+    return true;
+}
