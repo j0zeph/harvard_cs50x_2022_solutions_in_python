4b5d9d4a297e9eaabfa1168362c2ab1a94cad1bd
j0zeph <notaj@tutanota.de>
2022-06-28 08:53:22 +0300

    Remove unnecessary functions

--- 1.Psets/0.Hello/1.hello.py
+++ 1.Psets/0.Hello/1.hello.py
@@ -1,14 +1,4 @@
 #!/usr/bin/env python3
 
-def main():
-    greet(input("What is your name? "))
-
-
-def greet(name: str) -> None:
-    """"Says hello to user, using their name in the greeting"""
-
-    print("\nHello {}\n".format(name))
-
-
-if __name__ == "__main__":
-    main()
+name = input("What is your name? ")
+print(f'hello {name}')
